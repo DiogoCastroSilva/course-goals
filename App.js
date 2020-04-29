7b67cb6655b9da06b297ebc20b093802e39b51db
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-04-29 14:22:20 +0100

    Starting react native project

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{padding: 50}}>
+    </View>
+  );
+}
+
