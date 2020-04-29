d1caf85b54f8269c466170faa223127d3815103d
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-04-29 19:26:01 +0100

    Add a scrolable view to display a list of items

--- App.js
+++ App.js
@@ -1,27 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={{padding: 50}}>
-      <View style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center'}}>
+  const [goal, setGoal] = useState('');
+  const [goals, setGoals] = useState([]);
 
-            <TextInput 
-              placeholder='Course Goals'
-              style={{
-                width: '80%',
-                borderColor: 'black',
-                borderWidth: 1,
-                padding: 10
-              }} />
-            
-            <Button title='Add' />
-      </View>
+  const goalInputHandler = (text) => {
+    setGoal(text);
+  };
+
+  const addGoal = () => {
+    setGoals(currentGoals => [...currentGoals, { key: Math.random().toString() , value: goal }]);
+  };
+
+  return (
+    <View style={styles.screen}>
+        <View style={styles.inputContainer}>
+              <TextInput 
+                placeholder='Course Goals'
+                style={styles.input}
+                onChangeText={goalInputHandler}
+                value={goal} />
+              
+              <Button title='Add' onPress={addGoal} />
+        </View>
+        <FlatList
+          data={goals}
+          renderItem={itemData => (
+            <View key={itemData.item.key} style={styles.goalItem}>
+                <Text>{itemData.item.value}</Text>
+            </View>
+        )} />
+      {/* <ScrollView style={styles.goalItems}>
+          {goals.map((goal, index) => (
+            <View key={index} style={styles.goalItem}>
+              <Text>{goal}</Text>
+            </View>
+          ))}
+      </ScrollView> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
+  goalItems: {
+    
+  },
+  goalItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+
+  }
+});
