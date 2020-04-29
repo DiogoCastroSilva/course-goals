be3d8ec455dcdb777a5b7f2a3b8245b225f019c5
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-04-29 14:28:37 +0100

    Inline style and flexbox

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={{padding: 50}}>
+      <View style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'}}>
+
+            <TextInput 
+              placeholder='Course Goals'
+              style={{
+                width: '80%',
+                borderColor: 'black',
+                borderWidth: 1,
+                padding: 10
+              }} />
+            
+            <Button title='Add' />
+      </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({});
